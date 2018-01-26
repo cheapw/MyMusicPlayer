467462c9ab3d75164d458075270fc9243e802fce
小林 尹 <cheapw@outlook.com>
2018-01-26 11:07:42 +0800

    添加项目文件。

---
+++ MyMusicPlayer/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MyMusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_open = new System.Windows.Forms.Button();
+            this.b_up = new System.Windows.Forms.Button();
+            this.b_next = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // b_open
+            // 
+            this.b_open.Font = new System.Drawing.Font("微软雅黑 Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.b_open.Location = new System.Drawing.Point(26, 22);
+            this.b_open.Name = "b_open";
+            this.b_open.Size = new System.Drawing.Size(146, 41);
+            this.b_open.TabIndex = 0;
+            this.b_open.Text = "添加音乐文件";
+            this.b_open.UseVisualStyleBackColor = true;
+            this.b_open.Click += new System.EventHandler(this.b_open_Click);
+            // 
+            // b_up
+            // 
+            this.b_up.Location = new System.Drawing.Point(198, 104);
+            this.b_up.Name = "b_up";
+            this.b_up.Size = new System.Drawing.Size(88, 36);
+            this.b_up.TabIndex = 2;
+            this.b_up.Text = "上一曲";
+            this.b_up.UseVisualStyleBackColor = true;
+            this.b_up.Click += new System.EventHandler(this.b_up_Click);
+            // 
+            // b_next
+            // 
+            this.b_next.Location = new System.Drawing.Point(198, 166);
+            this.b_next.Name = "b_next";
+            this.b_next.Size = new System.Drawing.Size(88, 36);
+            this.b_next.TabIndex = 2;
+            this.b_next.Text = "下一曲";
+            this.b_next.UseVisualStyleBackColor = true;
+            this.b_next.Click += new System.EventHandler(this.b_next_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(26, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(146, 144);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(298, 275);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.b_next);
+            this.Controls.Add(this.b_up);
+            this.Controls.Add(this.b_open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b_open;
+        private System.Windows.Forms.Button b_up;
+        private System.Windows.Forms.Button b_next;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ MyMusicPlayer/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;  //Path类用到
+using System.Media;  //SoundPlayer类所在的命名空间
+using NAudio;
+using NAudio.Wave;
+
+namespace MyMusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> listSongs = new List<string>();//用来存储文件的全路径
+
+        private void b_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择音乐文件"; //打开的对话框所显示的标题
+            ofd.Multiselect = true;//设置多选
+            ofd.InitialDirectory = "F:\\cheapw\\Music"; //设置打开对话框的初始目录
+            ofd.Filter = "MP3音频文件|*.MP3|WAV音频文件|*.WAV|所有文件|*.*"; //设置文件格式帅选
+            ofd.ShowDialog();//显示打开文件的对话框
+            string[] pa_th = ofd.FileNames;//获得在文件夹中选择的所有文件的全路径
+            FileInfo file01 = new FileInfo("D:\\Music01.wav");
+            FileInfo file02 = new FileInfo("D:\\Music02.wav");
+            FileInfo file03 = new FileInfo("D:\\Music03.wav");
+            //for (int i = 0; i < pa_th.Length; i++)
+            //{
+            //    listBox1.Items.Add(Path.GetFileName(pa_th[i]));//将音乐文件夹的文件名加载到listBox中
+
+            //    listSongs.Add(pa_th[i]); //将音乐文件的全路径存储到泛型集合中
+            //}
+            string[] wavfile = new string[3];
+            wavfile[0] = file01.FullName;
+            wavfile[1] = file02.FullName;
+            wavfile[2] = file03.FullName;
+            for (int i = 0; i < pa_th.Length; i++)
+            {
+                
+                using (Mp3FileReader reader = new Mp3FileReader(pa_th[i]))
+                {
+                    using (WaveStream pcmStream = WaveFormatConversionStream.CreatePcmStream(reader))
+                    {
+                        WaveFileWriter.CreateWaveFile(wavfile[i], pcmStream);
+                    }
+                }
+            }
+            for (int i = 0; i < wavfile.Length; i++)
+            {
+                listBox1.Items.Add(Path.GetFileName(wavfile[i]));//将音乐文件夹的文件名加载到listBox中
+
+                listSongs.Add(wavfile[i]); //将音乐文件的全路径存储到泛型集合中
+            }
+        }
+
+        //实现双击播放
+        SoundPlayer sp = new SoundPlayer();
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = listSongs[listBox1.SelectedIndex];
+            sp.Play();
+        }
+        private void b_up_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b_next_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
